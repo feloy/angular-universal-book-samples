0fdde0bdd06dd935344de44bbf995aa4b8a70bfc
pmartin <contact@elol.fr>
2018-01-13 18:30:16 +0100

    ch03: Debug create-pages.sh to work with spaces in url

--- ng-universal-ch03/create-pages.sh
+++ ng-universal-ch03/create-pages.sh
@@ -3,9 +3,9 @@
 ng build --prod --app browser
 ng build --prod --app server --output-hashing=false
 webpack --config webpack.server.config.js
-for i in / $(cat sitemap.txt); do 
-  node dist/server.js dist/browser/index.html dist/universal $i
-done
+while read line; do
+  node dist/server.js dist/browser/index.html dist/universal "$line"
+done < sitemap.txt
 (
     cd dist/browser
     mv index.html .index.html
